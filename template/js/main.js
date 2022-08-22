cb77cb25f165256ae5de394bcc1d0061d2815d71
tomiwa abiola <tomiwabiola02@gmail.com>
2022-08-22 01:02:52 +0100

    addition of grid to my commit page. also the toggle to order button of ourstory page

--- template/js/main.js
+++ template/js/main.js
@@ -1,1 +1,7 @@
+let andi = document.querySelector('#innerButton')
 
+document.querySelector('.sampleOfCakeHidden').addEventListener('click',expandBestSellerOrderButton)
+
+function expandBestSellerOrderButton(){
+    andi.classList.toggle('hidden')
+}
